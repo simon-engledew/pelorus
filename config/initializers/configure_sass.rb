493011f08eb2c837a8c22c0f04ea2917d1c67cec
Simon Engledew <simon.engledew@ist.co.uk>
2010-09-25 20:56:18 +0100

    moved to scss

--- config/initializers/configure_sass.rb
+++ config/initializers/configure_sass.rb
@@ -1,2 +1,9 @@
 Sass::Plugin.options[:template_location] = File.join('app', 'stylesheets')
 Sass::Plugin.options[:style] = Rails.env.development?? :expanded : :compressed
+
+module Sass::Script::Functions
+  def numeric_value(pixels)
+    assert_type pixels, :Number
+    Sass::Script::String.new(pixels.coerce(['px'], []).value.to_s)
+  end
+end
