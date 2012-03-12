1294d8f74afae12d2fc646cc67286ff033619f54
Simon Engledew <simon.engledew@gmail.com>
2012-03-12 18:21:02 +0000

    try again

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,15 @@ class User < ActiveRecord::Base
   end
   
   # :http_authenticatable, :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :registerable, :database_authenticatable, :confirmable, :recoverable,
-         :rememberable, :trackable, :validatable
+  devise :registerable, :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable
+
+  validates_presence_of   :email
+  validates_uniqueness_of :email, :scope => :subdomain, :allow_blank => true
+  validates_format_of     :email, :with  => EMAIL_REGEX, :allow_blank => true
+
+  validates_presence_of     :password
+  validates_confirmation_of :password
+  validates_length_of       :password, :within => 6..20, :allow_blank => true
 
   attr_accessible :email, :name, :password, :password_confirmation, :admin
 end
