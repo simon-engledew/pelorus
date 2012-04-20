427ec4ad93d720fe6574ddd30e11bb4d59878f72
Simon Engledew <simon.engledew@gmail.com>
2012-04-20 18:36:01 +0100

    calm the logger down

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,6 +10,10 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.log_level = :info
+
+ActiveSupport::Deprecation.silenced = true
+
 # config.action_view.cache_template_loading            = true
 
 # Use a different cache store in production
